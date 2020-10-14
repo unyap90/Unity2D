d70ed4445501dae3d35a5dd86bc5dab3960a8767
Kobe <mail@limcheeyeap.info>
2020-10-14 08:00:00 +0800

    Check In player shooting

---
+++ Assets/weapon.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class weapon : MonoBehaviour
+{
+    public Transform FirePoint;
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        Instantiate(bulletPrefab, FirePoint.position, FirePoint.rotation);
+    }
+}
